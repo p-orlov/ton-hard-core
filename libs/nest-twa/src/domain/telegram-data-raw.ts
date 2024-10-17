37378b67bfe54b91f34139669ff928860424480d
Pavel <p.orlov.dev@gmail.com>
2024-10-17 10:34:33 +0400

    fix: telegram raw data

--- libs/nest-twa/src/domain/telegram-data-raw.ts
+++ libs/nest-twa/src/domain/telegram-data-raw.ts
@@ -1,5 +1,5 @@
 export type TelegramDataRaw = Readonly<{
-  // query_id?: string;
+  query_id?: string;
   user?: string;
   // receiver: WebAppUser
   // chat	WebAppChat
@@ -7,7 +7,7 @@ export type TelegramDataRaw = Readonly<{
   // chat_instance?: string;
   // start_param?: string;
   // can_send_after?: number;
-  // auth_date!: number;
+  auth_date: string;
   hash: string;
 }>;
 
@@ -20,8 +20,10 @@ export const TelegramDataRaw = {
     const parsed = Object.fromEntries(params);
 
     return {
+      auth_date: parsed['auth_date'],
       hash: parsed['hash'],
-      user: parsed['user']
+      user: parsed['user'],
+      query_id: parsed['query_id']
     };
   }
 };
